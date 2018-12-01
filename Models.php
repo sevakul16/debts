e0366c5ff80739dc84c97309322758c4bd618a4c
Vsevolod <sevakul16@gmail.com>
2018-12-01 10:12:19 +0200

    update

--- Models.php
+++ Models.php
@@ -1,7 +1,13 @@
 <?php
 require 'vendor/autoload.php';
-$db = new \atk4\data\Persistence_SQL('mysql:dbname=heroku_33a09646a43f60a;host=eu-cdbr-west-02.cleardb.net','b40ba71796d5af','a0bf7181');
+//$db = new \atk4\data\Persistence_SQL('mysql:dbname=heroku_33a09646a43f60a;host=eu-cdbr-west-02.cleardb.net','b40ba71796d5af','a0bf7181');
 
+
+if (isset($_ENV['CLEARDB_DATABASE_URL'])) {
+     $db = \atk4\data\Persistence::connect($_ENV['CLEARDB_DATABASE_URL']);
+ } else {
+     $db = \atk4\data\Persistence::connect('mysql:host=127.0.0.1;dbname=debts;charset=utf8', 'root', '');
+ }
 
 Class Client extends \atk4\data\Model {
   public $table ='client_seva';
